a6e7649f4b2c0f8e65204eb8ddf6eb594b03bbfe
Akash <gsaisriram.akash@gmail.com>
2021-01-31 20:50:32 +0530

    first commit

---
+++ client/src/components/TextContainer/TextContainer.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import onlineIcon from '../../icons/onlineIcon.png';
+
+import './TextContainer.css';
+
+const TextContainer = ({ users }) => (
+  <div className="textContainer">
+    <div>
+      <h1>Realtime Chat Application</h1>
+    </div>
+    {
+      users
+        ? (
+          <div>
+            <h1>People currently chatting:</h1>
+            <div className="activeContainer">
+              <h2>
+                {users.map(({name}) => (
+                  <div key={name} className="activeItem">
+                    {
+                      name.charAt(0).toUpperCase() + name.slice(1)
+                      }
+                    <img alt="Online Icon" src={onlineIcon}/>
+                  </div>
+                ))}
+              </h2>
+            </div>
+          </div>
+        )
+        : null
+    }
+  </div>
+);
+
+export default TextContainer;
